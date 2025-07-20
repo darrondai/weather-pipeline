dec605530b6e38dc4085fb0e7d0fb38e807ba5e9
Darron Dai <daidarron@gmail.com>
2025-07-20 13:56:42 -0700

    Move main.py entrypoint to top level directory

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import pandas as pd
 import psycopg2
 
-from extract import extract_weather_data
-from load import load_to_db
-from transform import transform_weather_data
+from src.extract import extract_weather_data
+from src.load import load_to_db
+from src.transform import transform_weather_data
 
 def main():
     OPEN_METEO_URL = 'https://historical-forecast-api.open-meteo.com/v1/forecast?latitude=37.7749&longitude=-122.4194&start_date=2025-07-15&end_date=2025-07-15&hourly=temperature_2m,apparent_temperature,uv_index,is_day,relative_humidity_2m&timezone=America%2FLos_Angeles&temperature_unit=fahrenheit'
