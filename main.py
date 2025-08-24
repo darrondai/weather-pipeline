15af2f4a5093613e9b60074918ff9161bcc55eb1
Darron Dai <daidarron@gmail.com>
2025-08-24 10:14:37 -0700

    Simplify main.py for testing

--- main.py
+++ main.py
@@ -7,10 +7,10 @@ from src.transform import transform_weather_data
 
 
 def main():
-    OPEN_METEO_URL = "https://historical-forecast-api.open-meteo.com/v1/forecast?latitude=37.7749&longitude=-122.4194&start_date=2025-07-16&end_date=2025-08-16&hourly=temperature_2m,apparent_temperature,uv_index,is_day,relative_humidity_2m&timezone=America%2FLos_Angeles&temperature_unit=fahrenheit"
-    raw_data = extract_weather_data(api_url=OPEN_METEO_URL)
+    # OPEN_METEO_URL = "https://historical-forecast-api.open-meteo.com/v1/forecast?latitude=37.7749&longitude=-122.4194&start_date=2025-07-16&end_date=2025-08-16&hourly=temperature_2m,apparent_temperature,uv_index,is_day,relative_humidity_2m&timezone=America%2FLos_Angeles&temperature_unit=fahrenheit"
+    # raw_data = extract_weather_data(api_url=OPEN_METEO_URL)
 
-    hourly_weather_df = transform_weather_data(raw_data)
+    # hourly_weather_df = transform_weather_data(raw_data)
 
     DB = "weather_db"
     USER = "postgres"
@@ -22,11 +22,12 @@ def main():
         database=DB, user=USER, password=PASSWORD, host=HOST, port=PORT
     ) as conn:
         with conn.cursor() as cur:
-            DB_TABLE = "sf_hourly_weather"
-            load_to_db(hourly_weather_df=hourly_weather_df, cursor=cur, table=DB_TABLE)
+            # DB_TABLE = "sf_hourly_weather"
+            # load_to_db(hourly_weather_df=hourly_weather_df, cursor=cur, table=DB_TABLE)
 
             cur.execute("SELECT * FROM sf_hourly_weather")
-            print(pd.DataFrame.from_records(cur.fetchall()))
+            # print(pd.DataFrame.from_records(cur.fetchall()))
+            print(cur.fetchall())
 
 
 if __name__ == "__main__":
