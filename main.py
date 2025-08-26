3462b879714aa42af0e560b06c2bbea37155eaaa
Darron Dai <daidarron@gmail.com>
2025-08-26 10:55:39 -0700

    Refactor load_to_db to use df.to_sql and module-scope engine

--- main.py
+++ main.py
@@ -5,29 +5,16 @@ from src.extract import extract_weather_data
 from src.load import load_to_db
 from src.transform import transform_weather_data
 
+from database.db import engine
+
 
 def main():
-    # OPEN_METEO_URL = "https://historical-forecast-api.open-meteo.com/v1/forecast?latitude=37.7749&longitude=-122.4194&start_date=2025-07-16&end_date=2025-08-16&hourly=temperature_2m,apparent_temperature,uv_index,is_day,relative_humidity_2m&timezone=America%2FLos_Angeles&temperature_unit=fahrenheit"
-    # raw_data = extract_weather_data(api_url=OPEN_METEO_URL)
+    OPEN_METEO_URL = "https://historical-forecast-api.open-meteo.com/v1/forecast?latitude=37.7749&longitude=-122.4194&start_date=2025-08-16&end_date=2025-08-16&hourly=temperature_2m,apparent_temperature,uv_index,is_day,relative_humidity_2m&timezone=America%2FLos_Angeles&temperature_unit=fahrenheit"
+    raw_data = extract_weather_data(api_url=OPEN_METEO_URL)
 
-    # hourly_weather_df = transform_weather_data(raw_data)
+    hourly_weather_df = transform_weather_data(raw_data)
 
-    DB = "weather_db"
-    USER = "postgres"
-    PASSWORD = "example"
-    HOST = "localhost"
-    PORT = 5432
-
-    with psycopg2.connect(
-        database=DB, user=USER, password=PASSWORD, host=HOST, port=PORT
-    ) as conn:
-        with conn.cursor() as cur:
-            # DB_TABLE = "sf_hourly_weather"
-            # load_to_db(hourly_weather_df=hourly_weather_df, cursor=cur, table=DB_TABLE)
-
-            cur.execute("SELECT * FROM sf_hourly_weather")
-            # print(pd.DataFrame.from_records(cur.fetchall()))
-            print(cur.fetchall())
+    load_to_db(hourly_weather_df, engine, "weather")
 
 
 if __name__ == "__main__":
--- src/load.py
+++ src/load.py
@@ -1,31 +1,8 @@
-from io import StringIO
-import psycopg2
-import psycopg2.sql as sql
 import pandas as pd
+from sqlalchemy.engine import Connectable
 
 
 def load_to_db(
-    hourly_weather_df: pd.DataFrame, cursor: psycopg2.extensions.cursor, table: str
+    df: pd.DataFrame, con: Connectable, table: str, if_exists: str = "append"
 ):
-    """Writes given df to a table on given database cursor"""
-    # create table if doesnt exist
-    create_table_query = sql.SQL("""
-        CREATE TABLE IF NOT EXISTS {table} (
-            temperature_2m_f REAL,
-            apparent_temperature_f REAL,
-            uv_index REAL,
-            is_day BOOLEAN,
-            relative_humidity_2m_perc REAL,
-            utc_time TIMESTAMPTZ PRIMARY KEY
-        )
-    """).format(table=sql.Identifier(table))
-
-    cursor.execute(create_table_query)
-
-    # read df into a csv buffer
-    with StringIO() as buffer:
-        hourly_weather_df.to_csv(buffer, index=False, header=False)
-        buffer.seek(0)
-
-        # copy csv buffer into table
-        cursor.copy_from(buffer, table=table, sep=",")
+    df.to_sql(name=table, con=con, if_exists=if_exists, index=False)
