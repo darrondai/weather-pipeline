2ea8b4d3202edfd42986af6ec1b793b9231306db
Darron Dai <daidarron@gmail.com>
2025-08-26 22:22:26 -0700

    Adjust model schema, refactor transform logic to fit

--- database/models.py
+++ database/models.py
@@ -9,11 +9,13 @@ from database.db import Base
 class Weather(Base):
     __tablename__ = "weather"
 
-    # latitude: Mapped[float] = mapped_column(Float, primary_key=True)
-    # longitude: Mapped[float] = mapped_column(Float, primary_key=True)
+    latitude: Mapped[float] = mapped_column(Float, primary_key=True)
+    longitude: Mapped[float] = mapped_column(Float, primary_key=True)
     temperature_2m_f: Mapped[float] = mapped_column(Float)
     apparent_temperature_f: Mapped[float] = mapped_column(Float)
-    uv_index: Mapped[float] = mapped_column(Float)
     is_day: Mapped[bool] = mapped_column(Boolean)
     relative_humidity_2m_perc: Mapped[int] = mapped_column(Integer)
-    utc_time: Mapped[datetime] = mapped_column(DateTime, primary_key=True)
+    utc_time: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True), primary_key=True
+    )
+    local_time: Mapped[datetime] = mapped_column(DateTime(timezone=True))
--- src/transform.py
+++ src/transform.py
@@ -4,26 +4,51 @@ import pandas as pd
 # convert the data into an hourly data frame (or timestamped by time)
 # each row is a time, and all of the hourly attributes at that time
 def transform_weather_data(response_data: dict) -> pd.DataFrame:
-    """Takes an open-meteo api response json in form of a python dict"""
+    flattened_df = pd.json_normalize(data=response_data)
 
-    # load raw response json into dataframe, including meta data
-    # each metric is an index, meta data are the columns
-    raw_df = pd.DataFrame.from_dict(data=response_data)
+    hourly_data = {
+        key: flattened_df.loc[0, key]
+        for key in flattened_df.columns
+        if "hourly." in key
+    }
 
-    # pivot weather metric lists into column entries
-    columns_to_extract = raw_df.index
-    hourly_data = {key: raw_df.at[key, "hourly"] for key in columns_to_extract}
-    hourly_df = pd.DataFrame(data=hourly_data)
+    # put latitude, longitude in each hourly data point
+    hourly_data["latitude"] = flattened_df.loc[0, "latitude"]
+    hourly_data["longitude"] = flattened_df.loc[0, "longitude"]
 
-    # parse local time into localized datetime
-    hourly_df["time"] = pd.to_datetime(hourly_df["time"])
-    original_timezone = raw_df["timezone"]["time"]
-    hourly_df["time"] = hourly_df["time"].dt.tz_localize(original_timezone)
+    hourly_df = pd.DataFrame.from_dict(hourly_data)
+
+    # rename flattened columns strip 'hourly.' prefix, add units to column names
+    hourly_df = hourly_df.rename(
+        columns={
+            "hourly.temperature_2m": "temperature_2m_f",
+            "hourly.time": "local_time",
+            "hourly.apparent_temperature": "apparent_temperature_f",
+            "hourly.is_day": "is_day",
+            "hourly.relative_humidity_2m": "relative_humidity_2m_perc",
+        }
+    )
+
+    # ENFORCE DATA TYPES AFTER LOADING
+    DTYPE_MAP = {
+        "latitude": float,
+        "longitude": float,
+        "is_day": bool,
+        "temperature_2m_f": float,
+        "apparent_temperature_f": float,
+        "relative_humidity_2m_perc": int,
+    }
+    hourly_df = hourly_df.astype(DTYPE_MAP)
+
+    # convert time strings to datetimes with timezone awareness
+    original_timezone = flattened_df.loc[0, "timezone"]
+
+    # parse time string into datetime, then make it timezone aware of the original timezone
+    hourly_df["local_time"] = pd.to_datetime(hourly_df["local_time"]).dt.tz_localize(
+        original_timezone
+    )
 
     # calc utc_time
-    hourly_df["utc_time"] = hourly_df["time"].dt.tz_convert("UTC")
-
-    # drop localized datetime
-    hourly_df.drop(columns=["time"], inplace=True)
+    hourly_df["utc_time"] = hourly_df["local_time"].dt.tz_convert("UTC")
 
     return hourly_df
