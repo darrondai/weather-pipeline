8ab1ac57f067da2d4d2288c24e67ef1101b13171
Darron Dai <daidarron@gmail.com>
2025-08-26 22:20:45 -0700

    Refactor to use new extract_weather_data

--- main.py
+++ main.py
@@ -9,8 +9,7 @@ from database.db import engine
 
 
 def main():
-    OPEN_METEO_URL = "https://historical-forecast-api.open-meteo.com/v1/forecast?latitude=37.7749&longitude=-122.4194&start_date=2025-08-16&end_date=2025-08-16&hourly=temperature_2m,apparent_temperature,uv_index,is_day,relative_humidity_2m&timezone=America%2FLos_Angeles&temperature_unit=fahrenheit"
-    raw_data = extract_weather_data(api_url=OPEN_METEO_URL)
+    raw_data = extract_weather_data()
 
     hourly_weather_df = transform_weather_data(raw_data)
 
