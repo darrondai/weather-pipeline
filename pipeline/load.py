1fb91e23efdc876f551467014dc441d741a1a1e1
Darron Dai <daidarron@gmail.com>
2025-08-27 09:33:34 -0700

    Refactor load step to ignore duplicate keys

--- pipeline/load.py
+++ pipeline/load.py
@@ -1,8 +1,14 @@
 import pandas as pd
-from sqlalchemy.engine import Connectable
+from sqlalchemy.engine import Engine
+from database.models import Weather
+from sqlalchemy.dialects.postgresql import insert
 
 
-def load_to_db(
-    df: pd.DataFrame, con: Connectable, table: str, if_exists: str = "append"
-):
-    df.to_sql(name=table, con=con, if_exists=if_exists, index=False)
+def load_to_db(df: pd.DataFrame, engine: Engine, table: str):
+    """Append the given dataframe into the corresponding database table. On primary key conflict, do nothing."""
+
+    records = df.to_dict(orient="records")
+    stmt = insert(Weather).values(records).on_conflict_do_nothing()
+
+    with engine.begin() as conn:
+        conn.execute(stmt)
